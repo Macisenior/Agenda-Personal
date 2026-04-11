1c9f2be0a0f1911fbacd4aa38f25f4572c3b8964
Macisenior <jlizqui@telefonica.net>
2026-04-12 00:40:40 +0200

    Mejora UI tareas

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,7 @@
-self.addEventListener("install", (e) => {
-  console.log("Service Worker instalado");
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  clients.claim();
 });
