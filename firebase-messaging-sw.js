5ce991b4ec3cd5fbd1965e434b450c696a647640
Macisenior <jlizqui@telefonica.net>
2026-04-12 21:50:15 +0200

    final alertas

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,17 @@
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-   apiKey: "AIzaSyAQzsTZxkHS5P5N1iqWBVVmrMbNMPQb2QQ",
-  authDomain: "agenda--app-c0220.firebaseapp.com",
-  projectId: "agenda--app-c0220",
-  storageBucket: "agenda--app-c0220.firebasestorage.app",
-  messagingSenderId: "1057225751235",
-  appId: "1:1057225751235:web:d4243cc400c593b6504076"
-});
+self.addEventListener("push", function (event) {
+  let data = {};
 
-const messaging = firebase.messaging();
+  try {
+    data = event.data.json();
+  } catch (e) {
+    return;
+  }
 
-messaging.onBackgroundMessage(function(payload) {
-  console.log("Mensaje recibido en background:", payload);
+  const payload = data.data || data;
 
-  const data = payload.data;
-
-  self.registration.showNotification(data.title, {
-    body: data.body,
+  self.registration.showNotification(payload.title || "Agenda", {
+    body: payload.body || "",
     icon: "/icon-192.png"
   });
 });
